625b1cd6cd03d4db9387dfe53d1a2a76ee89fda9
Pedro Rodrigues <peddroprp@gmail.com>
2020-04-08 10:27:41 -0300

    Add nodemon config

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (request, response) => {
-    return response.send('Hello World');
+    return response.send('Testing nodemon');
 });
 
 app.listen(3333);
