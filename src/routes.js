4eefd4c1d82ca1161677f3598fc35f74ea4a8a56
Pedro Rodrigues <peddroprp@gmail.com>
2020-04-08 11:37:32 -0300

    Setup database connection and post,get methods

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,31 @@
-const express = require('express')
-
+const express = require('express');
+const crypto = require('crypto')
 const routes = express.Router();
+const connection = require('./database/connection')
 
-routes.get('/', (request, response) => {
-    return response.send('Testing nodemon');
+routes.post('/ongs', async (request, response) => {
+    const { name, email, whatsapp, city, uf } = request.body;
+
+    const id = crypto.randomBytes(4).toString('HEX');
+
+    await connection('ongs').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf
+    });
+
+    return response.json({ id });
 });
+
+
+
+routes.get('/ongs', async (request, response) => {
+    const ongs = await connection('ongs').select('*');
+    
+    return response.json(ongs);
+})
 
 module.exports = routes;
