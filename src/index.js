346c2f2f330edc43b9eedb7f35876da85c8be1da
Pedro Rodrigues <peddroprp@gmail.com>
2020-04-08 11:16:32 -0300

    Setup database

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const routes = require('./routes')
+
 const app = express();
 
 app.use(express.json());
-
-app.get('/', (request, response) => {
-    return response.send('Testing nodemon');
-});
+app.use(routes);
 
 app.listen(3333);
+
