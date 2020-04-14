72dca6518581defb8ca8df489a6c89b56b5e5dd2
Pedro Rodrigues <peddroprp@gmail.com>
2020-04-14 17:35:16 -0300

    Implement State concept

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header'   
 
 function App() {
+  
+  const [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter + 1);
+  }
+  
+  
   return (
-    <Header>Be The Hero</Header>
+    <div>
+      <Header>Contador: {counter}</Header>
+      <button onClick={increment}>incrementar</button>
+    </div>
   );
 }
 
