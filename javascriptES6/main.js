5154aa3aa66f69b9724a93f0ba307a40c8027e75
Pedro Rodrigues <peddroprp@gmail.com>
2020-06-12 19:59:24 -0300

    class in js

--- javascriptES6/main.js
+++ javascriptES6/main.js
@@ -1,9 +1,25 @@
-alert('Test')
-
-class Test {
-    method() {
-
+class List {
+    constructor(){
+        this.list = [];
     }
 
-    test() {}
+    add(data){
+        this.list.push(data);
+        console.log(this.list);
+    }
+}
+
+
+class TodoList extends List{
+    constructor(){
+       super();
+
+       this.user = 'John';
+    }
+}
+
+var myTodoList = new TodoList();
+
+document.querySelector('#addButton').onclick = function() {
+    myTodoList.add('Make coffe');
 }
