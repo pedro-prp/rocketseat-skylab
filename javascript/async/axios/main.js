76b4b025c3ab248d3283db60ece4dd0355cec5c8
Pedro Rodrigues <peddroprp@gmail.com>
2020-06-12 19:42:58 -0300

    Setup basic structure and babel

--- javascript/async/axios/main.js
+++ javascript/async/axios/main.js
@@ -1,6 +1,6 @@
 axios.get('https://api.github.com/users/pedro-prp')
     .then(function(response) {
-        console.log(response);
+        console.log(response.data.repos_url);
     })
     .catch(function(err) {
         console.warn(err);
---
+++ javascriptES6/main.js
@@ -0,0 +1,9 @@
+alert('Test')
+
+class Test {
+    method() {
+
+    }
+
+    test() {}
+}
