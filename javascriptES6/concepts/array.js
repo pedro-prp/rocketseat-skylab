aa03f2770e982512e3d6256859c2c3a065cbb31e
Pedro Rodrigues <peddroprp@gmail.com>
2020-06-16 13:14:31 -0300

    refat Arrow manipulations with arrow functions

--- javascriptES6/concepts/array.js
+++ javascriptES6/concepts/array.js
@@ -1,28 +1,20 @@
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const mapArr = arr.map(function(item, index){
+const mapArr = arr.map((item, index) => {
     return item * index;
 });
-
 console.log(mapArr);
 
 
-const sumArr = arr.reduce(function(sum, next) {
-    return sum + next;
-});
-
+const sumArr = arr.reduce((sum, next) => sum + next);
 console.log(sumArr);
 
 
-const filterArr = arr.filter(function(item) {
-    return item % 2 === 0;
-});
-
+const filterArr = arr.filter(item => item % 2 == 0);
 console.log(filterArr);
 
 
-const findArr = arr.find(function(item) {
+const findArr = arr.find(item => {
     return item === 4;
 });
-
 console.log(findArr);
