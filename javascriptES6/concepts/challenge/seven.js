13bb41b658156ea6b13d30b364c8d7fafeaed6c1
Pedro Rodrigues <peddroprp@gmail.com>
2020-06-17 14:30:41 -0300

    Add exercice seven of concepts challenge

---
+++ javascriptES6/concepts/challenge/seven.js
@@ -0,0 +1,16 @@
+const nome = 'Diego';
+const idade = 23;
+
+// const usuario = {
+//     nome: nome,
+//     idade: idade,
+//     cidade: 'Rio do Sul',
+// };
+
+const user = {
+    nome,
+    idade,
+    cidade: 'Rio do Sul'
+}
+
+console.log(user);
