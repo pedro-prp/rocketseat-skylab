5a6ef8b362f7f5be1daf11c82a283994c8bcce72
Pedro Rodrigues <peddroprp@gmail.com>
2020-06-24 16:05:40 -0300

    Refactors product component to react hook

--- webreact/src/pages/product/index.js
+++ webreact/src/pages/product/index.js
@@ -1,36 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import {useParams } from 'react-router';
 
 import Api from '../../services/api';
 
 import './style.css'
 
-export default class Product extends Component {
-    state = {
-        product: {},
-    };
 
-    async componentDidMount() {
-        const { id } = this.props.match.params;
+export default function Product() {
+    const [product, setProduct] = useState({});
+    const { id } = useParams();
 
+    async function getProduct(id) {
         const response = await Api.get(`/products/${id}`);
 
-        this.setState({ product: response.data });
+        setProduct(response.data);
     }
 
+    useEffect(() => {
+        getProduct(id)
+    }, []);
 
-    render() {
-        const { product } = this.state;
 
-        return (
-            <div className="product-info">
-                <h1>{product.title}</h1>
-                <p>{product.description}</p>
+    return (
+        <div className="product-info">
+            <h1>{product.title}</h1>
+            <p>{product.description}</p>
 
-                <p>
-                    URL: <a href={product.url}>{product.url}</a>
-                </p>
-            </div>
-        )
-    }
-    
+            <p>
+                URL: <a href={product.url}>{product.url}</a>
+            </p>
+        </div>
+    )
+
+
 }
