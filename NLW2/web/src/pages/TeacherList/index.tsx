8877bbb8c78e22e55f572903e4647417227f5ed3
Pedro Rodrigues <peddroprp@gmail.com>
2020-08-04 15:44:50 -0300

    Create page header component

--- NLW2/web/src/pages/TeacherList/index.tsx
+++ NLW2/web/src/pages/TeacherList/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import PageHeader from '../../components/PageHeader';
+
+import './styles.css';
 
 function TeacherList() {
-    return <h1>teacher</h1>
+    return (
+        <div id="page-teacher-list" className="container">
+            <PageHeader title="Estes são os Proffys disponíveis"/>
+        </div>
+    )
 }
 
 export default TeacherList;
